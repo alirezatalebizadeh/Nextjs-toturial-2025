aa50cbf3d03add373505e64b4b31f6d31f27a66a
Alireza Talebizadeh <Alireza.talebizadeh78@gmail.com>
2025-01-05 19:54:07 +0330

    create basic structure of project

---
+++ app/dashboard/nav-links.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {
+    UserGroupIcon,
+    HomeIcon,
+    DocumentDuplicateIcon,
+  } from '@heroicons/react/24/outline';
+
+function NavLinks() {
+  return (
+    <div>NavLinks</div>
+  )
+}
+
+export default NavLinks
